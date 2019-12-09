e0b187759a21b60a7812a8ea6b2c1c4179cc3892
Daniel Weinman <namniewd@gmail.com>
2019-12-09 07:04:10 -0500

    finished day 9

--- day9/part1.js
+++ day9/part1.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 
 let input = fs.readFileSync('input.txt').toString().split(',').map(num => parseInt(num));
 
-console.log(input);
-
 let pc = 0;
 let relBase = 0;
 
@@ -20,11 +18,12 @@ while (input[pc] !== 99) {
   // determine mode of parameter, 3rd parameter mode always 0
   let modeParam1 = opString.length > 2 ? parseInt(opString[opString.length - 3]) : 0;
   let modeParam2 = opString.length > 3 ? parseInt(opString[opString.length - 4]) : 0;
+  let modeParam3 = opString.length > 4 ? parseInt(opString[opString.length - 5]) : 0;
 
   // determine parameters based on their mode
   let operandA;
   let operandB;
-  let operandC = input[pc + 3];
+  let operandC;
 
   switch(modeParam1) {
     case 0:
@@ -50,8 +49,17 @@ while (input[pc] !== 99) {
       break;
   }
 
+  switch(modeParam3) {
+    case 0:
+      operandC = input[pc+3] || 0;
+      break;
+    case 2:
+      operandC = input[pc+3] + relBase || 0;
+      break;
+  }
+
   // debug trace
-  console.log(`pc: ${pc}, input[pc], ${input[pc]}, opString: ${opString}, opCode: ${opCode}, mode1: ${modeParam1}, mode2: ${modeParam2}, operandA: ${operandA}, operandB: ${operandB}, operandC: ${operandC}, relBase: ${relBase}`)
+  // console.log(`pc: ${pc}, input[pc], ${input[pc]}, opString: ${opString}, opCode: ${opCode}, mode1: ${modeParam1}, mode2: ${modeParam2}, mode3: ${modeParam3}, operandA: ${operandA}, operandB: ${operandB}, operandC: ${operandC}, relBase: ${relBase}`)
 
   switch(opCode) {
     case 1:
@@ -65,7 +73,6 @@ while (input[pc] !== 99) {
     case 3:
       if (modeParam1 === 2) {
         input[input[pc+1] + relBase] = programStartInput;
-        console.log(`Wrote program input to position ${input[pc+1] + relBase}`)
       } else if (modeParam1 === 1) {
         input[input[pc+1]] = programStartInput
       }
